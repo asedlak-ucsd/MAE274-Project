5e162e8d6b6ca12ddb514cba7ff3d634f453aa4c
asedlak-ucsd <asedlak@ucsd.edu>
2025-02-22 15:40:10 -0800

    Added first order sp

--- SingularPerturbation.m
+++ SingularPerturbation.m
@@ -42,7 +42,7 @@ classdef SingularPerturbation
           hold off;
       end
 
-      function rsys = getrom(obj, rstates)
+      function rsys = getrom(obj, rstates, order)
           % Perform a zero-order singular perturbation reduction to 
           % remove the states in rstates from the state-space model
 
@@ -62,16 +62,32 @@ classdef SingularPerturbation
           B_f = B_cells{2};
           C_s = C_cells{1};
           C_f = C_cells{2};
+          
+          if order == "zero"
+              A_inv = inv(A_ff);
+              T1 = A_inv * A_fs;
+              T2 = A_inv * B_f;
+              Ar = A_ss - A_sf * T1;
+              Br = B_s - A_sf * T2;
+              Cr = C_s - C_f * T1;
+              Dr = psys.D - C_f * T2;
+    
+              rsys = ss(Ar, Br, Cr, Dr);
+          end
+          if order == "first"
+             A_inv = inv(A_ff);
+             T1 = A_inv^2 * A_fs;
+             T2 = inv(eye(ns) + A_sf*T1);
+             T3 = A_sf*A_inv;
 
-          A_inv = inv(A_ff);
-          T1 = A_inv * A_fs;
-          T2 = A_inv * B_f;
-          Ar = A_ss - A_sf * T1;
-          Br = B_s - A_sf * T2;
-          Cr = C_s - C_f * T1;
-          Dr = psys.D - C_f * T2;
+             Ar = T2*(A_ss - T3*A_fs);
+             Br = T2*(B_s - T3*B_f);
 
-          rsys = ss(Ar, Br, Cr, Dr);
+             Cr = C_s - C_f*(T1*Ar + A_inv*A_fs);
+             Dr = psys.D - C_f*(T1*Br + A_inv*B_f);
+
+             rsys = ss(Ar, Br, Cr, Dr);
+          end
       end
    end
 
--- src.m
+++ src.m
@@ -2,9 +2,31 @@ clear
 grid = load('grid.mat');
 gfl = load('gfl_src.mat');
 
-sp = SingularPerturbation(gfl.sys);
+sp = SingularPerturbation(grid.sys);
 
 view(sp)
-rsys = getrom(sp, [7 8]);
 
-sigma(gfl.sys, rsys, 'r--')
+%% ROM for the first GFLI
+
+inputs = [5 6];
+outputs = inputs;
+
+rsys = getrom(sp, [1:20 31:68], "first");
+
+opts = bodeoptions;
+opts.PhaseWrapping = 'on';
+opts.PhaseWrappingBranch =2;
+
+bodeplot(grid.sys(inputs, outputs), opts);
+hold on;
+bodeplot(rsys(inputs, outputs), 'r--', opts);
+hold off;
+
+%% 
+sigma(grid.sys(inputs, outputs), rsys(inputs, outputs), 'r--')
+100*norm(rsys(inputs, outputs) - grid.sys(inputs, outputs), Inf) / norm(grid.sys(inputs, outputs), Inf)
+
+
+%% TODO: 
+% - Check eigenvalues of ROM
+% - Iteratively reducing should not affect ROM, check this
